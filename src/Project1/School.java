83543f3c0ac2533ea586aaee01c59c20237c8691
AjShirin <shirinajmi@gmail.com>
2022-10-26 16:32:24 +0400

    add print statement in menue

--- src/Project1/School.java
+++ src/Project1/School.java
@@ -47,6 +47,7 @@ int a =1;
 		System.out.println("6. Coding problem ");
 		System.out.println("7. Currency Converotr To OMR ");
 		System.out.println("8. Exit the System");
+		System.out.println("***************************");
 
 	}
 
